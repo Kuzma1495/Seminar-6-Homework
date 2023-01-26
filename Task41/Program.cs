f3ec8a0fd125ec42a4c257fabbe8f4799c21031a
Kuzma <kuzmalebedev@yandex.ru>
2023-01-26 22:13:50 +0400

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+int Positive (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0 || array[i] == 0)
+        {
+            count = count + 1;
+        }
+    return count;
+}
+
+Console.Clear();
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine(Positive(array));
+
